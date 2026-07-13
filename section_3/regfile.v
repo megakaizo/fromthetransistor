280b300ec6c75501a40ccf07389f9bd9f885d769
thetrueryan <georgytoper@gmail.com>
2026-07-13 18:30:04 +0400

    section3: switch from arm7 to RISC-V, add CSR file

--- section_3/regfile.v
+++ section_3/regfile.v
@@ -2,35 +2,20 @@ module regfile(
  input wire clk,
  input wire we3,
  input wire [4:0] ra1, ra2, wa3,
- input wire rcpsr,
- input wire rspsr,
  input  wire [31:0] wd3,
- input wire [4:0] mode,
 
- output reg [31:0] rd1, rd2
+ output wire [31:0] rd1, rd2
 );
 
-// GENERAL REGS
-reg [31:0] r_g [16];
+reg [31:0] rf [32];
 
-// BANKING REGS
-reg [31:0] r_fiq [8:14];
-reg [31:0] r_svc, r_abt, r_irq, r_und [13:14];
+always @(posedge clk) begin
+ if (we3) begin
+  rf[wa3] <= wd3;
+ end
+end
 
-// STATUS REGS
-reg [31:0] cpsr;
-
-// BANKING STATUS REGS
-reg [31:0] spsr_fiq, spsr_svc, spsr_abt, spsr_irq, spsr_und;
-
-// CPU MODES
-parameter reg ABT_MODE = 5'b10100;
-parameter reg FIQ_MODE = 5'b10001;
-parameter reg IRQ_MODE = 5'b10010;
-parameter reg SVC_MODE = 5'b10011;
-parameter reg SYS_MODE = 5'b10101;
-parameter reg USR_MODE = 5'b10000;
-
-
+assign rd1 = (ra1 != 0) ? rf[ra1] : 0;
+assign rd2 = (ra2 != 0) ? rf[ra2] : 0;
 
 endmodule
